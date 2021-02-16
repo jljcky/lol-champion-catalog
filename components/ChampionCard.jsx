2746b1c95d4be8b718fe2283796e768eb1045ded
unknown <lojacky123@gmail.com>
2021-02-15 20:14:17 -0500

    small css changes in index

--- components/ChampionCard.jsx
+++ components/ChampionCard.jsx
@@ -1,14 +1,26 @@
 import Link from 'next/link';
 import styles from './championcard.module.css';
 
-const ChampionCard = ({ champion }) => {
+const ChampionCard = ({ champion, isMobile, latestVersion }) => {
     return (
         <Link href={`/champions/${champion.id}`}>
             <a className={styles["card-container"]}>
-                <div>
-                    <img style={{ width: '150px', height: '200px', objectFit: 'cover', objectPosition: '0 0' }} src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg`} />
-                    <h2>{champion.name}</h2>
-                </div>
+                {
+                    isMobile ? (
+                        <div style={{ height: '100px' }}>
+                            <img style={{ width: '100px', height: '100px' }} src={`http://ddragon.leagueoflegends.com/cdn/${latestVersion}/img/champion/${champion.id}.png`} />
+                            <div className={styles['card-border']} style={{ width: '96px', height: '96px' }} />
+                        </div>
+                    )
+                        :
+                        (
+                            <div>
+                                <img style={{ width: '150px', height: '200px', objectFit: 'cover', objectPosition: '0 0' }} src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${champion.id}_0.jpg`} />
+                                <h3>{champion.name}</h3>
+                                <div className={styles['card-border']} style={{ width: '146px', height: '196px' }} />
+                            </div>
+                        )
+                }
             </a>
         </Link>);
 }
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,11 @@ import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
+import {useMediaQuery} from 'react-responsive';
+
 import ChampionCard from '../components/ChampionCard';
 import {getAllChampionData, getLatestVersion} from '../lib/champions';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 // splash: 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg'
 // loading: 'http://ddragon.leagueoflegends.com/cdn/img/champion/loading/Aatrox_0.jpg'
@@ -12,18 +14,23 @@ import { useEffect, useState } from 'react';
 // passive: 'http://ddragon.leagueoflegends.com/cdn/11.3.1/img/passive/Anivia_P.png'
 // spells: 'http://ddragon.leagueoflegends.com/cdn/11.3.1/img/spell/FlashFrost.png'
 
-export default function Home({champions}) {
+export default function Home({champions, latestVersion}) {
   const [searchTerm, setSearchTerm] = useState("");
+  const isMobile = useMediaQuery({
+    query: '(max-device-width: 513px)'
+  })
 
   return (
-    <div>
+    <div className={styles.container}>
       <h1>League of Legends Champion Catalog</h1>
       <input className={styles.searchbar} type="text" placeholder="Search for a Champion...." value={searchTerm} onChange={(e) => {setSearchTerm(e.target.value)}}/>
       <div className={styles.catalog}>
         <div className={styles['catalog-container']}>
           {champions.map((champion) => {
             if (champion.name.toLowerCase().includes(searchTerm.toLowerCase()))
-              return <ChampionCard key={champion.key} champion={champion}/>
+              return (
+              <ChampionCard key={champion.key} champion={champion} latestVersion={latestVersion} isMobile={isMobile}/>
+              )
           })}
         </div>
       </div>
@@ -33,10 +40,12 @@ export default function Home({champions}) {
 
 export const getStaticProps = async () => {
   const allChampionData = await getAllChampionData();
+  const latestVersion = await getLatestVersion();
 
   return {
     props: {
-      champions: allChampionData
+      champions: allChampionData,
+      latestVersion
     }
   }
 }
